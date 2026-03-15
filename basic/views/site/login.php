32752930a4387bf67190b9dd3f58388ac3b4d891
Benitheappel <fr1cktobi07@gmail.com>
2026-03-15 21:56:43 +0100

    Improved the UI

--- basic/views/site/login.php
+++ basic/views/site/login.php
@@ -2,7 +2,6 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
-
 /** @var app\models\LoginForm $model */
 
 use yii\bootstrap5\ActiveForm;
@@ -11,37 +10,32 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="auth-card">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+        <?php $form = ActiveForm::begin([
+            'id' => 'login-form',
+            'fieldConfig' => [
+                'template' => "{label}\n{input}\n{error}",
+                'labelOptions' => ['class' => 'form-label'],
+                'inputOptions' => ['class' => 'form-control'],
+                'errorOptions' => ['class' => 'invalid-feedback'],
+            ],
+        ]); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Benutzername eingeben']) ?>
 
-            <?php $form = ActiveForm::begin([
-                    'id' => 'login-form',
-                    'fieldConfig' => [
-                            'template' => "{label}\n{input}\n{error}",
-                            'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                            'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                    ],
-            ]); ?>
+        <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Passwort eingeben']) ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'rememberMe')->checkbox([
+            'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+        ]) ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
-
-            <?= $form->field($model, 'rememberMe')->checkbox([
-                    'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            ]) ?>
-
-            <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                <?= Html::a('Registrieren', ['site/register'], ['class' => 'btn btn-secondary']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group mt-4">
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            <?= Html::a('Noch kein Account? Registrieren', ['site/register'], ['class' => 'btn btn-secondary']) ?>
         </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
--- basic/views/site/register.php
+++ basic/views/site/register.php
@@ -1,26 +1,37 @@
 <?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap5\ActiveForm $form */
+/** @var app\models\RegisterForm $model */
+
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
 
 $this->title = 'Registrieren';
 ?>
 <div class="site-register">
-    <h1>Registrieren</h1>
+    <div class="auth-card">
+        <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin([
+            'id' => 'register-form',
+            'fieldConfig' => [
+                'template' => "{label}\n{input}\n{error}",
+                'labelOptions' => ['class' => 'form-label'],
+                'inputOptions' => ['class' => 'form-control'],
+                'errorOptions' => ['class' => 'invalid-feedback'],
+            ],
+        ]); ?>
 
-            <?= $form->field($model, 'benutzername')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'benutzername')->textInput(['autofocus' => true, 'placeholder' => 'Gewünschten Benutzernamen wählen']) ?>
 
-            <?= $form->field($model, 'passwort')->passwordInput() ?>
+        <?= $form->field($model, 'passwort')->passwordInput(['placeholder' => 'Sicheres Passwort wählen']) ?>
 
-            <div class="form-group">
-                <?= Html::submitButton('Registrieren', ['class' => 'btn btn-success']) ?>
-                <?= Html::a('Zurück zum Login', ['site/login'], ['class' => 'btn btn-secondary']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="form-group mt-4">
+            <?= Html::submitButton('Registrieren', ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Bereits einen Account? Zum Login', ['site/login'], ['class' => 'btn btn-secondary']) ?>
         </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
